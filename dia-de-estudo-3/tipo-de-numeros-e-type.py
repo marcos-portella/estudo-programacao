b77d323d8918122e6182cae0dc7cbe3286af88f3
Marcos Portella <marcoscesargps@gmail.com>
2025-05-08 21:32:45 -0300

    primeira parte dia 3

---
+++ dia-de-estudo-3/tipo-de-numeros-e-type.py
@@ -0,0 +1,27 @@
+# Tipos de int e float
+
+# int -> Número inteiro
+'''
+ O tipo int representa qualquer número 
+ positivo ou negativo. 
+ int sem sinal é considerado positivo.
+'''
+print(11) # Int
+print(-11) # Int
+print(0) # Int
+
+
+#float -> Número com ponto flutuante
+'''
+O tipo float representa qualquer número
+positivo ou negativo com pontonflutuante.
+float sem sinal é considerado positivo.
+'''
+print(1.1, 10.11)
+
+
+# A função type mostra o  tipo que o python inferiu o valor.
+print(type('Marcos'))
+print(type(1))
+print(type(-1))
+print(type(1.1), type(-1.1), type(-1.1), type(0.0))
