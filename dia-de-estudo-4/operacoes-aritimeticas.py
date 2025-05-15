916a41a0f8ce31f598aaa185d1a0d7e680493d06
Marcos Portella <marcoscesargps@gmail.com>
2025-05-15 06:36:19 -0300

    Operaçãoes aritiméticas

---
+++ dia-de-estudo-4/operacoes-aritimeticas.py
@@ -0,0 +1,23 @@
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Esponeciação', exponenciacao)
+
+modulo = 55 % 2
+print('Módulo', modulo) # Bom para saber se um número é divisível por outro
+print(10 % 8 == 0) # Confirmando se é divisível
+print(16 % 8 == 0) 
+
