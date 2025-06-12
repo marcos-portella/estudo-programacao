11f8bb7cbfa8be22834b0895bbc0d001ae3c2301
Marcos Portella <marcoscesargps@gmail.com>
2025-06-12 17:28:20 -0300

    for in for,  list coprehesion

--- dia-de-estudo-18/list-comprehension/for-in-for-list-comprehesion.py
+++ dia-de-estudo-18/list-comprehension/for-in-for-list-comprehesion.py
@@ -1,14 +1,22 @@
-lista = []
+lista = [] # Método básico
 for x in range(3):
     for y in range(3):
         lista.append((x, y))
-lista = [
+print(lista)
+print()
+
+
+lista = [ # Usando o list comprehesion
     (x, y)
     for x in range(3)
     for y in range(3)
 ]
+print(lista)
+print()
+
+
 lista = [
-    [(x, letra) for letra in 'Luiz']
+    [(x, letra) for letra in 'Luiz'] # Um list comprehension dentro de outro, complexo até demais
     for x in range(3)
 ]
 
