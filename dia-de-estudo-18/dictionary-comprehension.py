86bcca7df0c58bf94c36163a206e9b9b499eb81d
Marcos Portella <marcoscesargps@gmail.com>
2025-06-12 17:44:49 -0300

    Dictionary Comprehension e Set Comprehension

---
+++ dia-de-estudo-18/dictionary-comprehension.py
@@ -0,0 +1,30 @@
+# Dictionary Comprehension e Set Comprehension
+produto = {
+    'nome': 'Caneta Azul',
+    'preco': 2.5,
+    'categoria': 'Escritório',
+}
+
+dc = { # o que vale para lista vale para o diconário
+    chave: valor.upper()
+    if isinstance(valor, str) else valor
+    for chave, valor
+    in produto.items()
+    if chave != 'categoria'
+}
+
+lista = [
+    ('a', 'valor a'),
+    ('b', 'valor a'),
+    ('b', 'valor a'),
+]
+dc = {
+    chave: valor
+    for chave, valor in lista
+}
+
+dict(lista) # Torna lista em dictionary
+
+
+s1 = {2 ** i for i in range(10)} # set comprehension
+print(s1)
