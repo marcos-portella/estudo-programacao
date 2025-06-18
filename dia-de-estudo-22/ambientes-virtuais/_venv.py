6da37f1b56e7a3dbdeff6ce54ceab3cd6bbc989b
Marcos Portella <marcoscesargps@gmail.com>
2025-06-18 18:31:15 -0300

    Funções recursivas e recursividade

---
+++ dia-de-estudo-22/ambientes-virtuais/_venv.py
@@ -0,0 +1,24 @@
+# Ambientes virtuais em Python (venv)
+# Um ambiente virtual carrega toda a sua instalação
+# do Python para uma pasta no caminho escolhido.
+# Ao ativar um ambiente virtual, a instalação do
+# ambiente virtual será usada.
+# venv é o módulo que vamos usar para
+# criar ambientes virtuais.
+# Você pode dar o nome que preferir para um
+# ambiente virtual, mas os mais comuns são:
+# venv env .venv .env
+
+
+# Meu caminho python
+#/c/Users/marco/AppData/Local/Programs/Python/Python313/python
+
+# Comando para ativar o ambiente virtual
+#. nome_do_ambiente/Scripts/activate
+
+# Comandos `pip`:
+# pip install nome_do_programa # instala o programa
+# pip uninstall nome_do_programa # desistala o programa
+# pip freeze # Exibe todos os programas instalados no seu ambiente virtual
+# pip -r nome_do_arquivo.txt # instala o que estiver em seu arquivo noambiente virtual
+# pip install nome_do_programa --upgrade # atualiza o programa para a versão mais recente
