a436fd2b43dab39bf0499a23c5aff9adee8e3889
Marcos Portella <marcoscesargps@gmail.com>
2025-06-22 17:09:21 -0300

    @property - um getter no modo Pythônico

---
+++ dia-de-estudo-25/property_+_setter.py
@@ -0,0 +1,39 @@
+# @property + @setter - getter e setter no modo Pythônico
+# - como getter
+# - p/ evitar quebrar código cliente
+# - p/ habilitar setter
+# - p/ executar ações ao obter um atributo
+# Atributos que começar com um ou dois underlines
+# não devem ser usados fora da classe.
+#  🐍🤓🤯🤯🤯🤯
+class Caneta:
+    def __init__(self, cor):
+        self.cor = cor
+        # private protected
+        self._cor_tampa = None
+
+    @property
+    def cor(self):
+        print('ESTOU NO GETTER')
+        return self._cor
+
+    @cor.setter
+    def cor(self, valor):
+        if valor == 'Rosa':
+            raise ValueError('Não aceito esta cor')
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
+
+caneta = Caneta('Azul')
+caneta.cor = 'Pink'
+caneta.cor_tampa = 'Azul'
+print(caneta.cor)
+print(caneta.cor_tampa)
