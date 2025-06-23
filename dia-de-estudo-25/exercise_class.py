5f182f2eb5a243b351746e538638f3c990ceb982
Marcos Portella <marcoscesargps@gmail.com>
2025-06-22 23:50:44 -0300

    Exercício com classes

---
+++ dia-de-estudo-25/exercise_class.py
@@ -0,0 +1,109 @@
+# Exercício com classes
+# 1 - Crie uma classe Carro (Nome)
+# 2 - Crie uma classe Motor (Nome)
+# 3 - Crie uma classe Fabricante (Nome)
+# 4 - Faça a ligação entre Carro tem um Motor
+# Obs.: Um motor pode ser de vários carros
+# 5 - Faça a ligação entre Carro e um Fabricante
+# Obs.: Um fabricante pode fabricar vários carros
+# Exiba o nome do carro, motor e fabricante na tela
+
+class car:
+    def __init__(self, name):
+        self.name = name
+        self.factory = None
+        self.engine = None
+
+    def set_factory(self, factory):
+        self.factory = factory
+
+    def set_engine(self, engine):
+        self.engine = engine
+
+    @classmethod
+    def create_car(cls, name,  factory, engine):
+        car = cls(name)
+        car.factory = factory
+        car.engine = engine
+        return car
+    
+class factory:
+    def __init__(self, name):
+        self.name = name
+
+class engine:
+    def __init__(self, name):
+        self.name = name
+
+factory1 = factory('foice')
+factory2 = factory('coice')
+
+engine1 = engine('pole')
+engine2 = engine('castro')
+
+car1 = car.create_car('Gato', factory1, engine1)
+car2 = car.create_car('lingui', factory2, engine2)
+
+print(car1.name, car1.factory.name, car1.engine.name)
+print(car2.name, car2.factory.name, car2.engine.name)
+
+
+# Solução do professor:
+
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+        self._motor = None
+        self._fabricante = None
+
+    @property
+    def motor(self):
+        return self._motor
+
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    @property
+    def fabricante(self):
+        return self._fabricante
+
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+
+class Motor:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+class Fabricante:
+    def __init__(self, nome):
+        self.nome = nome
+
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+
+gol = Carro('Gol')
+gol.fabricante = volkswagen
+gol.motor = motor_1_0
+print(gol.nome, gol.fabricante.nome, gol.motor.nome)
+
+fiat_uno = Carro('Uno')
+fiat = Fabricante('Fiat')
+fiat_uno.fabricante = fiat
+fiat_uno.motor = motor_1_0
+print(fiat_uno.nome, fiat_uno.fabricante.nome, fiat_uno.motor.nome)
+
+focus = Carro('Focus Titanium')
+ford = Fabricante('Ford')
+motor_2_0 = Motor('2.0')
+focus.fabricante = ford
+focus.motor = motor_2_0
+print(focus.nome, focus.fabricante.nome, focus.motor.nome)
