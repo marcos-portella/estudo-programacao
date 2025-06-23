8993542aa309ad797ed1f1fe9d269a1780989765
Marcos Portella <marcoscesargps@gmail.com>
2025-06-22 22:18:42 -0300

    Relações entre classes: associação, agregação e composição

---
+++ dia-de-estudo-25/associacao_agregacao_composicao.py
@@ -0,0 +1,135 @@
+# Relações entre classes: associação, agregação e composição
+# Associação é um tipo de relação onde os objetos
+# estão ligados dentro do sistema.
+# Essa é a relação mais comum entre objetos e tem subconjuntos
+# como agregação e composição (que veremos depois).
+# Geralmente, temos uma associação quando um objeto tem
+# um atributo que referencia outro objeto.
+# A associação não especifica como um objeto controla
+# o ciclo de vida de outro objeto.
+
+# Associação:
+class Escritor:
+    def __init__(self, nome) -> None:
+        self.nome = nome
+        self._ferramenta = None
+
+    @property
+    def ferramenta(self):
+        return self._ferramenta
+
+    @ferramenta.setter
+    def ferramenta(self, ferramenta):
+        self._ferramenta = ferramenta
+
+
+class FerramentaDeEscrever:
+    def __init__(self, nome):
+        self.nome = nome
+
+    def escrever(self):
+        return f'{self.nome} está escrevendo'
+
+
+escritor = Escritor('Luiz')
+caneta = FerramentaDeEscrever('Caneta Bic')
+maquina_de_escrever = FerramentaDeEscrever('Máquina')
+escritor.ferramenta = maquina_de_escrever
+
+print(caneta.escrever())
+print(maquina_de_escrever.escrever())
+print(escritor.ferramenta.escrever())
+
+
+# Agregação:
+
+# Agregação é uma forma mais especializada de associação
+# entre dois ou mais objetos. Cada objeto terá
+# seu ciclo de vida independente.
+# Geralmente é uma relação de um para muitos, onde um
+# objeto tem um ou muitos objetos.
+# Os objetos podem viver separadamente, mas pode
+# se tratar de uma relação onde um objeto precisa de
+# outro para fazer determinada tarefa.
+# (existem controvérsias sobre as definições de agregação).
+  
+class Carrinho:
+    def __init__(self):
+        self._produtos = []
+
+    def total(self):
+        return sum([p.preco for p in self._produtos])
+
+    def inserir_produtos(self, *produtos):
+        # self._produtos.extend(produtos)
+        # self._produtos += produtos
+        for produto in produtos:
+            self._produtos.append(produto)
+
+    def listar_produtos(self):
+        print()
+        for produto in self._produtos:
+            print(produto.nome, produto.preco)
+        print()
+
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+
+carrinho = Carrinho()
+p1, p2 = Produto('Caneta', 1.20), Produto('Camiseta', 20)
+carrinho.inserir_produtos(p1, p2)
+carrinho.listar_produtos()
+print(carrinho.total())
+
+
+# Composição
+
+# Composição é uma especialização da agregação.
+# Mas nela, quando o objeto "pai" for apagado, todas
+# as referências dos objetos filhos também são
+# apagadas.
+
+class Cliente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.enderecos = []
+
+    def inserir_endereco(self, rua, numero):
+        self.enderecos.append(Endereco(rua, numero))
+
+    def inserir_endereco_externo(self, endereco):
+        self.enderecos.append(endereco)
+
+    def listar_enderecos(self):
+        for endereco in self.enderecos:
+            print(endereco.rua, endereco.numero)
+
+    def __del__(self):
+        print('APAGANDO,', self.nome)
+
+
+class Endereco:
+    def __init__(self, rua, numero):
+        self.rua = rua
+        self.numero = numero
+
+    def __del__(self):
+        print('APAGANDO,', self.rua, self.numero)
+
+
+cliente1 = Cliente('Maria')
+cliente1.inserir_endereco('Av Brasil', 54)
+cliente1.inserir_endereco('Rua B', 6745)
+endereco_externo = Endereco('Av Saudade', 123213)
+cliente1.inserir_endereco_externo(endereco_externo)
+cliente1.listar_enderecos()
+
+del cliente1
+
+
+print(endereco_externo.rua, endereco_externo.numero)
+print('######################## AQUI TERMINA MEU CÓDIGO')
