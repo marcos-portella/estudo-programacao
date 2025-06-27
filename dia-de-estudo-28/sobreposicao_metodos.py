79adb99f42506b703a60f0e7b27d71c5ea76ea4e
Marcos Portella <marcoscesargps@gmail.com>
2025-06-27 17:26:54 -0300

    docs: add OOP examples with super() and MRO

--- dia-de-estudo-28/sobreposicao_metodos.py
+++ dia-de-estudo-28/sobreposicao_metodos.py
@@ -1,8 +1,4 @@
-# super() e a sobreposição de membros - Python Orientado a Objetos
-# Classe principal (Pessoa)
-#   -> super class, base class, parent class
-# Classes filhas (Cliente)
-#   -> sub class, child class, derived class
+
 
 
 class MinhaString(str):
