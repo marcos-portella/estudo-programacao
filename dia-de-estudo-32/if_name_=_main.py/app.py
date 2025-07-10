da0a3538226aa093089498409d98bea59375a59e
Marcos Portella <marcoscesargps@gmail.com>
2025-07-10 20:39:25 -0300

    feat: Demonstrate `if __name__ == '__main__'` for module execution control
    * **`if __name__ == '__main__'`**: Explores the use of `if __name__ == '__main__'` to control code execution when a module is run directly versus imported.
    * **Module Structure (`modulo.py`)**: Defines a `soma` function with type hints. Includes a test case within the `if __name__ == '__main__'` block, emphasizing that this block only runs when `modulo.py` is executed as the main script.
    * **Module Import (`app.py`)**: Shows how `app.py` imports the `soma` function from `modulo.py`. When `app.py` is run, the code inside `if __name__ == '__main__'` in `modulo.py` is not executed, demonstrating its role in preventing unintended side effects upon import.
    * **Learning Goal**: Illustrates how `if __name__ == '__main__'` is crucial for creating reusable modules and organizing Python code.

---
+++ dia-de-estudo-32/if_name_=_main.py/app.py
@@ -0,0 +1,3 @@
+from modulo import soma
+
+print(soma(4, 2))
---
+++ dia-de-estudo-32/if_name_=_main.py/modulo.py
@@ -0,0 +1,6 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+if __name__ == '__main__':  # Não é bom fazer testes assim, faça automação
+    print(soma(10, 30))
