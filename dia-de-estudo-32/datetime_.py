dfe9f44b02de7512617910ce073791b2189b6f5a
Marcos Portella <marcoscesargps@gmail.com>
2025-07-10 20:41:35 -0300

    feat: Master Python `datetime` for date and time manipulation
    * **Date & Time Creation**: Demonstrates creating `datetime` objects using `datetime(year, month, day, ...)` and parsing strings with `datetime.strptime('DATA', 'FORMATO')`.
    * **Current Time & Timestamp**: Shows how to get the current local time using `datetime.now()` and convert it to a Unix timestamp.
    * **Timestamp Conversion**: Illustrates converting a Unix timestamp back to a `datetime` object using `datetime.fromtimestamp()`.
    * **Learning Goal**: Provides a didactic overview of core `datetime` functionalities for versatile date and time handling in Python, including basic creation, current time, and timestamp conversions.

--- dia-de-estudo-32/datetime_.py
+++ dia-de-estudo-32/datetime_.py
@@ -17,6 +17,7 @@ from datetime import datetime
 # data_str_data = '20/04/2022'
 # data_str_fmt = '%d/%m/%Y'
 
+
 # data = datetime(2022, 4, 20, 7, 49, 23)
 # data = datetime.strptime(data_str_data, data_str_fmt)
 
